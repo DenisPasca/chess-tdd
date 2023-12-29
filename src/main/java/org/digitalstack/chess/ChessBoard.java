2a6bfb28bc66d31b8715f9c308cfe11574b2c007
DenisPasca <pascadenis1997@gmail.com>
2023-12-29 12:01:06 +0200

    Implemented required methods s

---
+++ src/main/java/org/digitalstack/chess/ChessBoard.java
@@ -0,0 +1,47 @@
+package org.digitalstack.chess;
+
+public class ChessBoard {
+
+    public static int BOARD_WIDTH = 7;
+    public static int BOARD_HEIGHT = 7;
+
+    private final Pawn[][] pieces;
+
+    public ChessBoard() {
+        pieces = new Pawn[BOARD_WIDTH][BOARD_HEIGHT];
+
+
+    }
+
+    public void add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
+
+        if (xCoordinate < BOARD_WIDTH && pieces[xCoordinate][yCoordinate] == null) {
+            pawn.setXCoordinate(xCoordinate);
+            pawn.setYCoordinate(yCoordinate);
+            pieces[xCoordinate][yCoordinate] = pawn;
+        }
+
+        else {
+            pawn.setXCoordinate(-1);
+            pawn.setYCoordinate(-1);
+        }
+
+
+//        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+    }
+
+    public boolean isLegalBoardPosition(int xCoordinate, int yCoordinate) {
+
+        if(xCoordinate>7||yCoordinate>7) {
+            return false;
+        }
+
+        if(xCoordinate<0||yCoordinate<0) {
+            return false;
+        }
+
+        return true;
+
+//        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+    }
+}
---
+++ src/main/java/org/digitalstack/chess/Pawn.java
@@ -0,0 +1,71 @@
+package org.digitalstack.chess;
+
+public class Pawn {
+
+    private ChessBoard chessBoard;
+    private int xCoordinate;
+    private int yCoordinate;
+    private PieceColor pieceColor;
+
+    public Pawn(PieceColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public ChessBoard getChesssBoard() {
+        return chessBoard;
+    }
+
+    public void setChessBoard(ChessBoard chessBoard) {
+        this.chessBoard = chessBoard;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setXCoordinate(int value) {
+        this.xCoordinate = value;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setYCoordinate(int value) {
+        this.yCoordinate = value;
+    }
+
+    public PieceColor getPieceColor() {
+        return this.pieceColor;
+    }
+
+    public void setPieceColor(PieceColor value) {
+        pieceColor = value;
+    }
+
+    public void move(MovementType movementType, int newX, int newY) {
+
+
+
+       if(movementType.equals(MovementType.MOVE)) {
+
+           if(!( newY-getYCoordinate()>=2 || !chessBoard.isLegalBoardPosition(newX, newY))) {
+
+               yCoordinate=newY;
+           }
+
+       }
+
+
+//        throw new UnsupportedOperationException("Need to implement Pawn.move()");
+    }
+
+    @Override
+    public String toString() {
+        return currentPositionAsString();
+    }
+
+    protected String currentPositionAsString() {
+        return String.format("Current X: %s\nCurrent Y: %s\nPiece Color: %s", xCoordinate, yCoordinate, pieceColor);
+    }
+}
